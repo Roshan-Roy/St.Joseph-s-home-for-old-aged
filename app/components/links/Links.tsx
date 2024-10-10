342228100c00413a4b1ca27378edb8894dce5312
Roshan-Roy <roshanroy2911@gmail.com>
2024-10-10 23:28:48 +0530

    finished

--- app/components/links/Links.tsx
+++ app/components/links/Links.tsx
@@ -5,7 +5,7 @@ import { usePathname } from "next/navigation";
 const Links = ({ name, path, func }: {
     name: string;
     path: string;
-    func: any;
+    func: () => {};
 }) => {
     const pathName = usePathname()
     return (
