7c2a32f040fb3068f86be0cb2b1626200460d7c8
Roshan-Roy <roshanroy2911@gmail.com>
2024-11-07 00:49:20 +0530

    gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export"
+    output:"export",
+    basePath: '/St.Joseph-s-home-for-old-aged'
 };
 
 export default nextConfig;
